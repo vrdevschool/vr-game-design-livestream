8c7462158d72e9d6716a83be97bfc861ae5261fb
Lee Wasilenko <lee.wasilenko@gmail.com>
2016-06-03 13:40:33 -0700

    Commit after Livestream on June 2nd 2016. We have the makings of a Plink game and a ball stacking game.

---
+++ Assets/Scripts/PickUpableObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpableObject : MonoBehaviour
+{
+    public bool b_PickedUp = false;
+
+    public float f_TimerEnd = 15f;
+    public float f_CurrentTimer = 0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (b_PickedUp)
+        {
+            f_CurrentTimer = 0f;
+        }
+        else
+        {
+            f_CurrentTimer = f_CurrentTimer + Time.deltaTime;
+
+            if (f_CurrentTimer >= f_TimerEnd)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/PickupParent.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+    public GameObject prefabSphere;
+    public GameObject prefabCube;
+    public GameObject prefabCylinder;
+
+
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void FixedUpdate () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Touch' on the Trigger");
+        }
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated TouchDown on the Trigger");
+        }
+
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated TouchUp on the Trigger");
+        }
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Press' on the Trigger");
+        }
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated PressDown on the Trigger");
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated PressUp on the Trigger");
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("You activated PressUp on the Touchpad");
+            GameObject.Instantiate(prefabSphere);
+            //sphere.transform.position = Vector3.zero;
+            //sphere.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            //sphere.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    void OnTriggerStay (Collider col)
+    {
+        Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have collided with " + col.name + " while holding down Touch");
+            col.attachedRigidbody.isKinematic = true;
+            col.gameObject.transform.SetParent(gameObject.transform);
+
+            PickUpableObject tempPickUpObject;
+            tempPickUpObject = col.GetComponent<PickUpableObject>();
+            tempPickUpObject.b_PickedUp = true;
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have released Touch while colliding with " + col.name);
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+
+            tossObject(col.attachedRigidbody);
+
+            PickUpableObject tempPickUpObject;
+            tempPickUpObject = col.GetComponent<PickUpableObject>();
+            tempPickUpObject.b_PickedUp = false;
+        }
+    }
+
+    void tossObject(Rigidbody rigidBody)
+    {
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if (origin != null)
+        {
+            rigidBody.velocity = origin.TransformVector(device.velocity);
+            rigidBody.angularVelocity = origin.TransformVector(device.angularVelocity);
+        } else
+        {
+            rigidBody.velocity = device.velocity;
+            rigidBody.angularVelocity = device.angularVelocity;
+        }
+        
+    }
+}
