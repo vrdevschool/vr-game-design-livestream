06bbc5d6045122748680e186af210da84ee600a2
eljaydub <lee.wasilenko@gmail.com>
2016-06-09 21:16:30 -0700

    End of June 9th Live Stream
    - Added Scorekeeper
    - Added Purple Shower Particle System
    - Game is now called 'Ball Stacker Shower'

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour
+{
+    public ScoreKeeper scoreKeeper;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("SOMETHING IS IN THE TRIGGER");
+
+        if ("Sphere".Equals(other.gameObject.tag))
+        {
+            scoreKeeper.UpdateScore(1);
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public Text scoreText;
+    public int MaxScore = 10;
+    public int CurrentScore = 0;
+
+    public ParticleSystem ps_YouWin;
+    
+    public void UpdateScore(int addedScoreAmount)
+    {
+        CurrentScore++;
+        scoreText.text = "SCORE: " + CurrentScore.ToString();
+
+        if(CurrentScore >= MaxScore)
+        {
+            YouWin();
+        }
+    }
+
+    public void YouWin()
+    {
+        ps_YouWin.Emit(1000);
+    }
+}
